088ebaf0742e09782d5868c2df3137004fdedce4
Francis Calimbas <66287856+francisCalimbas@users.noreply.github.com>
2021-12-06 03:11:41 +0800

    Create controller.js

---
+++ controllers/controller.js
@@ -0,0 +1,157 @@
+const mysql = require('../db')
+
+const db = mysql.db;
+
+const controller = {
+
+  getHome: (req, res) => {
+    console.log("Home")
+    const data = {
+      styles: ['style'],
+      scripts: [ 'home','navbar'],
+      title: "Home Page" // title of the web page
+    }
+    res.render('home', data)
+  },
+  getDirectors: (req, res) => {
+    console.log("Directors")
+    const data = {
+      styles: ['style'],
+      scripts: [ 'navbar','director'],
+      title: "Directors" // title of the web page
+    }
+    res.render('director', data)
+  },
+
+  getMovies: (req, res) => {
+    console.log("Movies")
+    const data = {
+      styles: ['style'],
+      scripts: [ 'navbar','movies'],
+      title: "Movies" // title of the web page
+    }
+    res.render('movies', data)
+  },
+
+  getGenres: (req, res) => {
+    console.log("Genres")
+    const data = {
+      styles: ['style'],
+      scripts: [ 'navbar','genre'],
+      title: "Genres" // title of the web page
+    }
+    res.render('genre', data)
+  },
+
+  fetchDirectors: (req, res) => {
+    /*
+      SELECT CONCAT(F.first_name, " ", F.last_name) AS director, COUNT(F.movie_name) AS movie_cnt
+      FROM fact_table F
+      JOIN movies_genres MG ON MG.movie_id = F.movie_id
+      WHERE F.year < 1980 AND MG.genre = 'Horror'
+      GROUP BY director
+      HAVING movie_cnt >= 2;
+
+      numofmovieschoice: formdata[0].value,
+      numofmoviesint: formdata[1].value,
+      releasedonschoice: formdata[2].value,
+      releasedonint: formdata[3].value,
+      genre:formdata[4].value
+    */
+   console.log('fetchdirec')
+    const choice = req.body.numofmovieschoice;
+    const numofmoviesint = req.body.numofmoviesint;
+    const releasedonschoice = req.body.releasedonschoice;
+    const releasedonint = req.body.releasedonint;
+    const genre = req.body.genre;
+
+    let query = 'SELECT CONCAT(`F`.first_name, " ", `F`.last_name) AS director, COUNT(`F`.movie_name) AS movie_cnt, `F`.`year` ' +
+                  'FROM fact_table `F` ' +
+                  'JOIN movies_genres `MG` ON `MG`.movie_id = `F`.movie_id ';
+    var whereFlag = 0;
+    // where
+    if (releasedonschoice === 'ATLEAST') {
+      query = query.concat('WHERE `F`.`year` >= ' + releasedonint);
+      whereFlag = 1;
+    } else if (releasedonschoice === 'EQUAL TO') {
+      query = query.concat('WHERE `F`.`year` = ' + releasedonint);
+      whereFlag = 1;
+    } else if (releasedonschoice === 'LESS THAN') {
+      query = query.concat('WHERE `F`.`year` < ' + releasedonint);
+      whereFlag = 1;
+    }
+    
+    // 2nd where
+    if (genre !== null && genre !== undefined && genre !== '') {
+      if (whereFlag == 0) {
+        query = query.concat('WHERE `MG`.`genre` = ' + '\'' + genre + '\'');
+      } else if (whereFlag == 1) {
+        query = query.concat(' AND `MG`.`genre` = ' + '\'' + genre + '\'');
+      }
+    }
+
+    query = query.concat(' GROUP BY director');
+    // having
+    if (choice === 'ATLEAST') {
+      query = query.concat(' HAVING `movie_cnt` >= ' + numofmoviesint);
+    } else if (choice === 'EQUAL TO') {
+      query = query.concat(' HAVING `movie_cnt` = ' + numofmoviesint);
+    } else if (choice === 'LESS THAN') {
+      query = query.concat(' HAVING `movie_cnt` < ' + numofmoviesint);
+    }
+
+    query = query + ' LIMIT 0, 10;';
+    console.log(query);
+    db.query(query, function(err, results, fields) {
+      if (err) console.log(err);
+      else {
+        console.log(results); // results contains rows returned by server
+        res.send(true);
+      }
+    })
+  },
+
+  fetchActors: (req, res) => {
+    /*
+      SELECT CONCAT(F.first_name, " ", F.last_name) AS director, COUNT(F.movie_name) AS movie_cnt, F.year
+      FROM fact_table F
+      JOIN movies_genres MG ON MG.movie_id = F.movie_id
+      GROUP BY director WITH ROLLUP;
+
+      numofmovieschoice: formdata[0].value,
+      numofmoviesint: formdata[1].value,
+      releasedonschoice: formdata[2].value,
+      releasedonint: formdata[3].value,
+      genre:formdata[4].value
+    */
+    const query = 'SELECT CONCAT(F.first_name, " ", F.last_name) AS director, COUNT(F.movie_name) AS movie_cnt, F.year' +
+                  'FROM fact_table F' +
+                  'JOIN movies_genres MG ON MG.movie_id = F.movie_id';
+    
+  },
+
+  fetchGenres: (req, res) => {
+    /*
+      SELECT CONCAT(F.first_name, " ", F.last_name) AS director, COUNT(F.movie_name) AS movie_cnt, F.year
+      FROM fact_table F
+      JOIN movies_genres MG ON MG.movie_id = F.movie_id
+      GROUP BY director WITH ROLLUP;
+
+      numofmovieschoice: formdata[0].value,
+      numofmoviesint: formdata[1].value,
+      releasedonschoice: formdata[2].value,
+      releasedonint: formdata[3].value,
+      genre:formdata[4].value
+    */
+    const query = 'SELECT CONCAT(F.first_name, " ", F.last_name) AS director, COUNT(F.movie_name) AS movie_cnt, F.year' +
+                  'FROM fact_table F' +
+                  'JOIN movies_genres MG ON MG.movie_id = F.movie_id';
+    
+  }
+
+
+
+
+}
+
+module.exports = controller
