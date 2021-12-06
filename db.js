6f79738a25703183c16688dbbc8b060865d929dc
Francis Calimbas <francis_calimbas@dlsu.edu.ph>
2021-12-06 21:36:44 +0800

    Update db.js

--- db.js
+++ db.js
@@ -12,18 +12,27 @@ const mysql = require("mysql2");
 // });
 
 //raven
+// const db = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "12345",
+//   port: "3310",
+//   database: "imdb_dw",
+// });
+
+// francis
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "12345",
-  port: "3310",
-  database: "imdb_dw",
+  port: "3306",
+  database: "imdb_dw1",
 });
 
 db.connect();
 
 // db.query(
-//     'SELECT * FROM fact_table LIMIT 0, 100',
+//     'SELECT * FROM fact_table LIMIT 0, 10',
 //     function(err, results, fields) {
 //       console.log(results); // results contains rows returned by server
 //     //   console.log(fields); // fields contains extra meta data about results, if available
